df09befa5d62516633cfa6503bd53199976a3794
Alexander Telegin <telegin.alexander@gmail.com>
2021-12-09 14:21:50 +0400

    Mint 2

--- scripts/mint-script.js
+++ scripts/mint-script.js
@@ -1,7 +1,7 @@
 const hre = require("hardhat");
 async function main() {
   const NFT = await hre.ethers.getContractFactory("NeurLoot");
-  const URI = "https://nft.neurloot.com/metadata/1.json"
+  const URI = "https://nft.neurloot.com/metadata/2.json"
   const WALLET_ADDRESS = "0xCFf5c2d2575BAe803CB8A7730CfD351bf22cd969"
   const CONTRACT_ADDRESS = "0x274Ee74012D8cCFddDa14cB5dddE79D0cDFA215A"
   const contract = NFT.attach(CONTRACT_ADDRESS);
