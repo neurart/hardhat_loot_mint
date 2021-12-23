f272cbc1cfae1976cdcb56af38078af4f3241a6a
Alexander Telegin <telegin.alexander@gmail.com>
2021-12-23 15:51:32 +0400

    WIP 2

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,9 +1,9 @@
 const hre = require("hardhat");
 async function main() {
   const NFT = await hre.ethers.getContractFactory("NeurLoot");
-  const URI = "https://nft.neurloot.com/metadata/10.json"
-  const WALLET_ADDRESS = "0xCFf5c2d2575BAe803CB8A7730CfD351bf22cd969"
-  const CONTRACT_ADDRESS = "0x8Be503bcdEd90ED42Eff31f56199399B2b0154CA"
+  const URI = "https://nft.neurloot.com/metadata/1.json"
+  const WALLET_ADDRESS = "0xf7a643F3Dfc4b49a06e30AfA349ae13873FF86BD"
+  const CONTRACT_ADDRESS = "0xCc64e1fFC33D7B99013AAb5D71E34886310040a0"
   const contract = NFT.attach(CONTRACT_ADDRESS);
   await contract.mint(WALLET_ADDRESS, URI);
   console.log("NFT minted:", contract);
