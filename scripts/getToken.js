87ec80b4aaa7686696058ed8fcd38072398038da
Alexander Telegin <telegin.alexander@gmail.com>
2021-12-23 15:00:52 +0400

    Revert 1

--- scripts/getToken.js
+++ scripts/getToken.js
@@ -1,16 +1,12 @@
 const hre = require("hardhat");
 async function main() {
   const NFT = await hre.ethers.getContractFactory("NeurLoot");
-  // OLD PROD: 0x274Ee74012D8cCFddDa14cB5dddE79D0cDFA215A
-  // FIRST TEST: 0xCE12589cB48E579F3cA419b70096fb34235ff7CF
-  const CONTRACT_ADDRESS = "0xBd770416a3345F91E4B34576cb804a576fa48EB1"
+  const URI = "https://nft.neurloot.com/metadata/10.json"
+  const WALLET_ADDRESS = "0xCFf5c2d2575BAe803CB8A7730CfD351bf22cd969"
+  const CONTRACT_ADDRESS = "0x274Ee74012D8cCFddDa14cB5dddE79D0cDFA215A"
   const contract = NFT.attach(CONTRACT_ADDRESS);
-  const id = 1;
-  const owner = await contract.ownerOf(id);
-  console.log("NFT: ", id);
-  // console.log("Owner: ", owner);
-  // const uri = await contract.tokenURI(id);
-  // console.log("URI: ", uri);
+  await contract.mint(WALLET_ADDRESS, URI);
+  console.log("NFT minted:", contract);
 }
 main().then(() => process.exit(0)).catch(error => {
   console.error(error);
--- scripts/mint.js
+++ scripts/mint.js
@@ -1,14 +1,11 @@
 const hre = require("hardhat");
 async function main() {
   const NFT = await hre.ethers.getContractFactory("NeurLoot");
-  // OLD PROD: 0x274Ee74012D8cCFddDa14cB5dddE79D0cDFA215A
-  // FIRST TEST: 0xCE12589cB48E579F3cA419b70096fb34235ff7CF
-  // 0x05DfF05a33aca5D190F8F78A47aeBaa002f55d31
-  const CONTRACT_ADDRESS = "0xBd770416a3345F91E4B34576cb804a576fa48EB1"
-  const WALLET = "0x2e039dbc68e81D1cA1344fBe4C5039f8c0799258"
+  const URI = "https://nft.neurloot.com/metadata/10.json"
+  const WALLET_ADDRESS = "0xCFf5c2d2575BAe803CB8A7730CfD351bf22cd969"
+  const CONTRACT_ADDRESS = "0x8Be503bcdEd90ED42Eff31f56199399B2b0154CA"
   const contract = NFT.attach(CONTRACT_ADDRESS);
-  // await contract.safeMint(WALLET, 1);
-  await contract.mint(WALLET, "uri");
+  await contract.mint(WALLET_ADDRESS, URI);
   console.log("NFT minted:", contract);
 }
 main().then(() => process.exit(0)).catch(error => {
